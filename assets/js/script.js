845e9a40bdca6b49c71dbd204da512b758330a3f
Lindelwa <lindelwa.codes@gmail.com>
2023-09-13 20:47:14 +0200

    Display book on the page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,21 @@
-const myLibrary = [];
+const myLibrary = ["Man Search For Meaning", "Rich Dad Poor Dad", "Atomic Habits", "Think and Grow Rich", "The Facebook Effect", "The Lean Startup", "Zero To One", "Obstacle Is The Way", "Spud: The Crazy Normal", "No Time For Goodbyes"];
 
 function Book() {
   // the constructor...
 }
 
 function addBookToLibrary(book) {
-    myLibrary.push(book);
+  myLibrary.push(book);
+}
+
+function displayBooks() {
+  const booksContainer = document.querySelector(".books-container");
+
+  for (const book of myLibrary) {
+    const card = document.createElement("div");
+    card.className = "card";
+    card.textContent = book;
+
+    booksContainer.appendChild(card);
+  }
 }
